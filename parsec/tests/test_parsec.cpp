5de3552ac541719f6ceccb8877565e969ce43bdf
Matthew O'Connell <matthew.david.oconnell@gmail.com>
2025-10-07 09:37:25 -0400

    chore: record manual deletions of legacy parsec/ and include/ps folders

--- parsec/tests/test_parsec.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_all.hpp>
-#include <ps/parsec.hpp>
-
-TEST_CASE("add adds two integers", "add") {
-    REQUIRE(ps::add(2, 3) == 5);
-    REQUIRE(ps::add(-1, 1) == 0);
-}
